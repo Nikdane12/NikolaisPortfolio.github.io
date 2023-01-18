52ffb276725c75a4328d092ec51e7aee3d86eb6f
Nikdane12 <nikolai.dickens00@gmail.com>
2023-01-18 19:24:49 +0100

    Updated Graphics Site W/ Anim

--- public/home/js/code.js
+++ public/home/js/code.js
@@ -1,42 +1,55 @@
 let menubox
 let menux
 let menustate
+let anim
 let currentTheme = localStorage.getItem('theme') ?? 'light'
 
 const tgl = () => {
-    currentTheme = currentTheme == 'dark' ? 'light' : 'dark'
-    document.documentElement.setAttribute('data-theme', currentTheme)
-    localStorage.setItem('theme', currentTheme)
+  currentTheme = currentTheme == 'dark' ? 'light' : 'dark'
+  document.documentElement.setAttribute('data-theme', currentTheme)
+  localStorage.setItem('theme', currentTheme)
 }
 document.documentElement.setAttribute('data-theme', currentTheme)
 
 
 const Start = () => {
-    menubox = document.getElementById('menubox')
-    menux = document.getElementById('menux')
-    menux.classList.remove('open')
-
+  bar = document.getElementById("bar")
+  menubox = document.getElementById('menubox')
+  menux = document.getElementById('menux')
+  anim = document.getElementsByClassName("anim");
+  menux.classList.remove('open')
+  animate()
+  window.onscroll = animate
 }
 
 const tglMenu = () => {
-    menustate = menustate == 'open' ? 'closed' : 'open'
-    if (menustate == 'open') {
-        menubox.style.left = `0px`
-        menux.classList.add('open')
-    }
-    else{
-        menubox.style.left = ``
-        menux.classList.remove('open')
-    }
+  menustate = menustate == 'open' ? 'closed' : 'open'
+  if (menustate == 'open') {
+    menubox.style.left = `0px`
+    menux.classList.add('open')
+  }
+  else {
+    menubox.style.left = ``
+    menux.classList.remove('open')
+  }
 
 }
 
-window.onscroll = function() {scrollFunction()}
-
-function scrollFunction() {
+const animate = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
-    document.getElementById("bar").classList.add('scrollstuff')
+    bar.classList.add('scrollstuff')
   } else {
-    document.getElementById("bar").classList.remove('scrollstuff')
+    bar.classList.remove('scrollstuff')
   }
-} 
+  for (var i = 0; i < anim.length; i++) {
+    let windowHeight = window.innerHeight
+    let elementTop = anim[i].getBoundingClientRect().top
+    let elementVisible = 150
+    if (elementTop < windowHeight - elementVisible) {
+      anim[i].classList.add("animstart")
+    } else {
+      anim[i].classList.remove("animstart")
+    }
+  }
+}
+
