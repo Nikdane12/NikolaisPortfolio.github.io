cd04b681ef4cf18a974e9dddfa8fa9079ff424d4
Nikdane12 <nikolai.dickens00@gmail.com>
2023-01-22 14:54:33 +0100

    AnimationTestEdit

--- public/HTMLexamples/animationtest.js
+++ public/HTMLexamples/animationtest.js
@@ -1,32 +1,86 @@
 let wrapper = document.getElementById('wrapper')
-let demo = document.getElementsByClassName('demo')
 
-let animation = anime({
-    targets: demo,
-    translateY: [
-      {value: 0, easing: 'easeOutSine', duration: 100},
-      {value: 100, easing: 'easeInSine', duration: 100}
-    ],
-    delay: anime.stagger(300, {easing: 'linear'}),
-    autoplay: false,
-  })
+let columns = 0, rows = 0, toggled = false
 
-
-  let play = false;
-
-function toggleAnimation() {
-  console.log(play)
-  if (play) {
-    console.log('justplayed');
-    animation.reverse()
-    console.log('reverse');
-  } else {
-    console.log('isplaying');
-    animation.play()
-    console.log('play');
-  }
-  play = !play;
-  setTimeout(function() {}, 200);
+const toggle = () => {
+  toggled = !toggled
 }
 
-wrapper.onclick = toggleAnimation
+
+const click = index => {
+  toggle()
+  if(toggled){
+    document.body.classList.add('toggled')
+  }
+  else{
+    document.body.classList.remove('toggled')
+  }
+  let tiles = anime({
+    targets: '.tile',
+    scale: {
+      value: toggled ? 0 : 1,
+      easing: 'easeInCubic',
+      duration: 300
+    },
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index ?? 0,
+    }),
+  })
+  anime({
+    targets: '.textanim',
+    opacity: {
+      value: toggled ? 1 : 0,
+      easing: 'easeInCubic',
+      duration: 500
+    },
+    delay: anime.stagger(1000, { start: 30 * Math.max(columns, rows) }),
+  })
+  anime({
+    targets: '.wordanim',
+    opacity: {
+      value: toggled ? 1 : 0,
+      easing: 'easeInCubic',
+      duration: 300,
+      delay: anime.stagger(200, { start: 30 * Math.max(columns, rows) + 500}),
+    },
+    translateY: {
+      value: [-130, 0],
+      easing: 'easeInSine',
+      duration: 600,
+      delay: anime.stagger(200, { start: 30 * Math.max(columns, rows) + 100})
+    },
+  })
+}
+
+
+const createTile = index => {
+  const tile = document.createElement("div")
+  tile.classList.add("tile")
+  tile.style.scale = toggled ? 0 : 1
+  tile.onclick = e => click(index)
+  return tile
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index))
+  })
+}
+
+
+const setGrid = () => {
+  wrapper.innerHTML = ""
+  const windowSize = document.body.clientWidth > 800 ? 100 : 50
+  columns = Math.floor(document.body.clientWidth / windowSize)
+  rows = Math.floor(document.body.clientHeight / windowSize)
+
+  wrapper.style.setProperty("--columns", columns)
+  wrapper.style.setProperty("--rows", rows)
+
+  createTiles(columns * rows)
+}
+
+setGrid()
+
+window.onresize = setGrid
