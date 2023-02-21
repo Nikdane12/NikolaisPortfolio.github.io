bf9076206657647148f30571b695b5cc6346d6dd
Nikdane12 <nikolai.dickens00@gmail.com>
2023-02-21 14:46:46 +0100

    ChessJS update (NOT final)

--- public/HTMLprojects/chess/chessJS.js
+++ public/HTMLprojects/chess/chessJS.js
@@ -1,10 +1,36 @@
+// https://github.com/jhlywa/chess.js/blob/master/README.md
+
 let blockcont = document.getElementById('blockcont')
 let pieceholder = document.getElementById('pieceholder')
 let dotholder = document.getElementById('dotholder')
-let selected = false
+let selectedElement
+let movesBlock 
 const chess = new Chess()
+// 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3'
 
-
+const startup = () => {
+    createTiles()
+    console.log(chess.ascii());
+    addPieces()
+    selectedElement = null
+    // give every block a onclick that selects itself
+    const singleblocks = Array.from(document.getElementsByClassName('singleblock'))
+    singleblocks.forEach((element, index) => {
+        element.onclick = () => {select(element)}
+    })
+    window.onresize = () => {
+        const piece = document.getElementsByClassName('piece')
+        const greendot = document.getElementsByClassName('Gdotcont')
+        Array.from(piece).forEach(element => {
+            const pieceBlock = document.querySelectorAll(`[data-pos="${element.dataset.piecepos}"]`)[0]
+            setPos(element, pieceBlock)
+        })
+        Array.from(greendot).forEach(element => {
+            const greendotplace = document.querySelectorAll(`[data-pos="${element.dataset.dotpos}"]`)[0]
+            setPos(element, greendotplace)
+        });
+    }
+}
 
 const createTiles = () => {
     for (let i = 0; i < 64; i++) {
@@ -21,100 +47,61 @@ const createTiles = () => {
     blockcont.style.setProperty("--columns", Math.sqrt(64))
 }
 
-createTiles()
-console.log(chess.ascii());
+const resetTiles = () => {
+    const tiles = document.querySelectorAll('.singleblock')
+    tiles.forEach(element => {
+        const square = chess.get(element.dataset.pos) || 'empty'
+        element.dataset.piecetype = square.type || 'empty'
+        element.dataset.piececolor = square.color || 'empty'
+    })
+}
+
+
 
 const findPos = (element) => {
     const wrapperPosX = blockcont.getBoundingClientRect().left
     const positionX = element.getBoundingClientRect().left
-    const distanceX = positionX + (element.clientWidth/2)
+    const distanceX = positionX + (element.clientWidth / 2)
     const wrapperPosY = blockcont.getBoundingClientRect().top
     const positionY = element.getBoundingClientRect().top
-    const distanceY = positionY + (element.clientHeight/2)
+    const distanceY = positionY + (element.clientHeight / 2)
     return [distanceX, distanceY]
 }
 
-const setPos = (oldpos , newpos) => {
-    const offsetX = (oldpos.clientWidth / 2)
-    const offsetY = (oldpos.clientHeight / 2) 
+const setPos = (currentpos, newpos) => {
+    const offsetX = (currentpos.clientWidth / 2)
+    const offsetY = (currentpos.clientHeight / 2)
     const [distanceX, distanceY] = findPos(newpos)
-    oldpos.style.transform = `translate(${distanceX - offsetX}px, ${distanceY - offsetY}px)`
+    currentpos.style.transform = `translate(${distanceX - offsetX}px, ${distanceY - offsetY}px)`
     // console.log(distanceX, -element.clientWidth, distanceY, -element.clientHeight);
     // element.style.top = distanceY - element.clientHeight + "px"
     // element.style.left = distanceX - element.clientWidth + "px"
 }
-
 const addPieces = () => {
-    const gamePieces = ['pawn', 'knight', 'bishop', 'rook', 'queen', 'king']
-    let injecter = `onload="SVGInject(this,{makeIdsUnique:false,useCache:false})"`
-    const pawnscont = document.querySelectorAll('div[data-piecetype="p"]')
-    pawnscont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${pawnscont[index].dataset.pos}" ></div>`
-        const piece = document.getElementsByClassName('p')[index]
-        setPos(piece, element)
+    ["p","k","n","b","q","r"].forEach(piecetype => {
+        const cont = document.querySelectorAll(`div[data-piecetype="${piecetype}"]`)
+        cont.forEach((element, index) => {
+            pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${cont[index].dataset.pos}" ></div>`
+            const piece = document.getElementsByClassName(piecetype)[index]
+            setPos(piece, element)
+        })
     })
-    const knightcont = document.querySelectorAll('div[data-piecetype="n"]')
-    knightcont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${knightcont[index].dataset.pos}" ></div>`
-        
-        const piece = document.getElementsByClassName('n')[index]
-        setPos(piece, element)
-
-    })
-    const bishopcont = document.querySelectorAll('div[data-piecetype="b"]')
-    bishopcont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${bishopcont[index].dataset.pos}" ></div>`
-        
-        const piece = document.getElementsByClassName('b')[index]
-        setPos(piece, element)
-
-    })
-    const rookcont = document.querySelectorAll('div[data-piecetype="r"]')
-    rookcont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${rookcont[index].dataset.pos}" ></div>`
-        
-        const piece = document.getElementsByClassName('r')[index]
-        setPos(piece, element)
-
-    })
-    const queencont = document.querySelectorAll('div[data-piecetype="q"]')
-    queencont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${queencont[index].dataset.pos}"  ></div>`
-        
-        const piece = document.getElementsByClassName('q')[index]
-        setPos(piece, element)
-
-    })
-    const kingcont = document.querySelectorAll('div[data-piecetype="k"]')
-    kingcont.forEach((element, index) => {
-        pieceholder.innerHTML += `<div class="piece ${element.dataset.piecetype} ${element.dataset.piececolor == 'w' ? 'white' : 'black'}" data-piecepos="${kingcont[index].dataset.pos}" ></div>`
-        
-        const piece = document.getElementsByClassName('k')[index]
-        setPos(piece, element)
-
-    })
-
-    
 }
-addPieces()
 
-let selectedElement = null
 
-const singleblocks = Array.from(document.getElementsByClassName('singleblock'))
-singleblocks.forEach((element, index) => {
-    element.onclick = () => {
-        console.log('pos:', element.dataset.pos, 'type:', element.dataset.piecetype, 'color:', element.dataset.piececolor)
-        console.log(ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }), 'noMoves'))
+
+
+const select = (element) => {
+    if (selectedElement == element) {
+        console.log('deselect', selectedElement);
+        deselect(selectedElement)
+    }
+    else {
+        console.log('else');
+        console.log('pos:', element.dataset.pos, 'type:', element.dataset.piecetype, 'color:', element.dataset.piececolor, 'moves:', ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }), 'noMoves'))
         const piece = document.querySelectorAll(`[data-piecepos="${element.dataset.pos}"]`)[0]
-        if (selectedElement) {
-            deselect(selectedElement)
-        }
-        else{
-            element.onclick = () => deselect(element)
-        }
-        if(!isundefined(piece)){piece.style.top = -10 + "px"}
-        if(ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }))){
-            console.log('balls');
+        if (!isundefined(piece)) { piece.style.top = -10 + "px" }
+        if (ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }))) {
             chess.moves({ square: element.dataset.pos }).forEach((value, index) => {
                 const div = document.createElement('div')
                 div.classList.add('Gdotcont')
@@ -123,88 +110,99 @@ singleblocks.forEach((element, index) => {
                 div.appendChild(greendot)
                 dotholder.appendChild(div)
                 const avalibleMoves = chess.moves({ square: element.dataset.pos })
-                let movesBlock = document.querySelectorAll(`div[data-pos="${avalibleMoves[index].slice(-2)}"]`)[0]
+                div.dataset.dotpos = cutStrings(avalibleMoves[index])
+                console.log(cutStrings(avalibleMoves[index]))
+                movesBlock = document.querySelectorAll(`div[data-pos="${cutStrings(avalibleMoves[index])}"]`)[0]
                 setPos(div, movesBlock)
-                movesBlock.onclick = () => {
-                    movesBlock.removeEventListener('click', () => {})
-                    movePiece(element.dataset.pos, avalibleMoves[index].slice(-2))
-                    movesBlock = null
+                console.log('green');
+                const movesBlockOnClick = () => {
+                    if (movesBlock) {
+                        movesBlock.removeEventListener('click', movesBlockOnClick)
+                        movePiece(element.dataset.pos, cutStrings(avalibleMoves[index]))
+                        movesBlock = null
+                    }
                 }
-    
-    
+                movesBlock.addEventListener('click', movesBlockOnClick)
             })
         }
-        
+        if (selectedElement) {
+            deselect(selectedElement)
+        }
+        element.onclick = () => {select(element)}
         selectedElement = element;
-    
-    }}
-)
+    }
+}
 
 
-
-// const select = (element) => {
-//     console.log('pos:', element.dataset.pos, 'type:', element.dataset.piecetype, 'color:', element.dataset.piececolor)
-//     console.log(ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }), 'noMoves'))
-//     const piece = document.querySelectorAll(`[data-piecepos="${element.dataset.pos}"]`)[0]
-//     if (selectedElement) {
-//         deselect(selectedElement)
-//     }
-//     else{
-//         element.onclick = () => deselect(element)
-//     }
-//     if(!isundefined(piece)){piece.style.top = -10 + "px"}
-//     if(ifArrayIsEmpty(chess.moves({ square: element.dataset.pos }))){
-//         console.log('balls');
-//         chess.moves({ square: element.dataset.pos }).forEach((value, index) => {
-//             const div = document.createElement('div')
-//             div.classList.add('Gdotcont')
-//             const greendot = document.createElement('div')
-//             greendot.classList.add("greendot")
-//             div.appendChild(greendot)
-//             dotholder.appendChild(div)
-//             const avalibleMoves = chess.moves({ square: element.dataset.pos })
-//             let movesBlock = document.querySelectorAll(`div[data-pos="${avalibleMoves[index].slice(-2)}"]`)[0]
-//             setPos(div, movesBlock)
-//             movesBlock.onclick = () => {
-//                 movesBlock.removeEventListener('click', () => {})
-//                 movePiece(element.dataset.pos, avalibleMoves[index].slice(-2))
-//                 movesBlock = null
-//             }
-
-
-//         })
-//     }
-    
-//     selectedElement = element;
-
-// }
 
 const movePiece = (from, to) => {
+    // deselect currently selected piece
     selectedElement = null
+    // use library to move pieces
     chess.move({ from: `${from}`, to: `${to}` })
-    console.log('move:',from , to);
+    console.log(chess.ascii());
+    console.log('move:', from, to);
     const element = document.querySelectorAll(`[data-piecepos="${from}"]`)[0]
-    console.log(element);
-    // element.dataset.piecepos = to
-    console.log(element);
-    const squareElement = document.querySelectorAll(`[data-pos="${to}"]`)[0]
-    setPos(element, squareElement)
+    const fromElement = document.querySelectorAll(`[data-pos="${from}"]`)[0]
+    const toElement = document.querySelectorAll(`[data-pos="${to}"]`)[0]
+    console.log(element, from);
+    // actually move the piece
+    setPos(element, toElement)
+    //update piece and block data
+    element.dataset.piecepos = to
+    fromElement.dataset.piecetype = chess.get(from) ? chess.get(from).type : 'empty'
+    fromElement.dataset.piececolor = chess.get(from) ? chess.get(from).color : 'empty'
+    toElement.dataset.piecetype = chess.get(to).type
+    toElement.dataset.piececolor = chess.get(to).color
+    // lower selected piece
     element.style.top = "0px"
-    console.log('removed green');
+    //remove avalible pieces
     removeChilderen(dotholder)
+    // change whos turn
+    spinTurn()
 }
 
 const deselect = (element) => {
-    console.log('deselect', element.dataset.pos);
-    const piece = document.querySelectorAll(`[data-piecepos="${element.dataset.pos}"]`)[0]
-    console.log(piece);
-    piece.style.top = "0px"
+    console.log('deselected');
+    // find whos piece is on this block
+    const foo = document.querySelectorAll(`[data-piecepos="${element.dataset.pos}"]`)[0]
+    // lower piece
+    foo.style.top = "0px"
+    // remove avalible pieces
     removeChilderen(dotholder)
+    // deselect piece
     selectedElement = null
-    element.onclick = () => {select(element)}
+    // reset onclick
+    element.onclick = () => { select(element) }
 }
 
+const reset = () => {
+    chess.reset()
+    removeChilderen(pieceholder);
+    resetTiles()
+    addPieces()
+    spinTurn()
+}
 
+const spinTurn = () => {
+    const spinner = document.getElementById('spinner')
+    if (chess.turn() == 'w') {
+        spinner.style.transform = "rotate(180deg)"
+    }
+    else {
+        spinner.style.transform = "rotate(0deg)"
+    }
+}
+
+const cutStrings = (str) => {
+    const arr = [];
+    for (let i = 0; i < 64; i++) {
+        const newNumber = Math.floor(i / 8) + 1;
+        const letter = String.fromCharCode(97 + i % 8);
+        arr.push(letter + newNumber);
+    }
+    return arr.find(value => str.includes(value))
+}
 
 const ifArrayIsEmpty = (Array, message) => {
     if (Array.length === 0) {
@@ -216,15 +214,16 @@ const ifArrayIsEmpty = (Array, message) => {
 }
 
 const removeChilderen = (element) => {
+    if(!element.hasChildNodes()){console.log('no child')}
     while (element.hasChildNodes()) {
         element.removeChild(element.firstChild)
     }
-    if(element.hasChildNodes()){console.log('removed green')}
-    if(!element.hasChildNodes()){console.log('no green')}
+    if(element.hasChildNodes()){console.log('removed')}
+    
 }
 
 const isundefined = (myVariable) => {
-    if(myVariable === undefined){return true}
-    else{return false}
+    if (myVariable === undefined) { return true }
+    else { return false }
 }
 
