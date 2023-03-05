b28ba58697f350856ccc14077135d155dece1f12
Nikdane12 <nikolai.dickens00@gmail.com>
2023-03-05 16:04:52 +0100

    Three JS stuff

--- public/home/js/cardcreator.js
+++ public/home/js/cardcreator.js
@@ -2,12 +2,12 @@ const HTMLprojects = {
     title: 'HTML Projects', 
     group: 'HTMLprojects',
     cards: [
-        { name: 'Notflix', image: 'NotflixThumbnail.png', link: 'notflix', datecreated: '04.11.23', tags: ['JS', 'API'],},
-        { name: 'Weather App', image: 'WeatherAppThumbnail.png', link: 'weather', datecreated: '29.11.22', tags: ['JS', 'API'],},
-        { name: 'Julekalander', image: 'JulekalanderThumbnail.png', link: 'julekalander', datecreated: '06.12.22', tags: ['JS', 'SCHOOL'],},
-        { name: 'Product Site', image: 'ProductSiteThumbnail.png', link: 'productsite', datecreated: '26.11.22', tags: ['JS']},
-        { name: 'Kino Site', image: 'KinoThumbnail.png', link: 'kinoprojekt/kinohome', datecreated: '20.10.22', tags: ['HTML', 'CSS', 'SCHOOL'],},
         { name: 'Chess App', image: 'ChessThumbnail.png', link: 'chessjs', datecreated: '07.02.23', tags: ['JS', 'LIBRARY'],},
+        { name: 'Julekalander', image: 'JulekalanderThumbnail.png', link: 'julekalander', datecreated: '06.12.22', tags: ['JS', 'SCHOOL'],},
+        { name: 'Weather App', image: 'WeatherAppThumbnail.png', link: 'weather', datecreated: '29.11.22', tags: ['JS', 'API'],},
+        { name: 'Product Site', image: 'ProductSiteThumbnail.png', link: 'productsite', datecreated: '26.11.22', tags: ['JS']},
+        { name: 'Notflix', image: 'NotflixThumbnail.png', link: 'notflix', datecreated: '04.11.22', tags: ['JS', 'API'],},
+        { name: 'Kino Site', image: 'KinoThumbnail.png', link: 'kinoprojekt/kinohome', datecreated: '20.10.22', tags: ['HTML', 'CSS', 'SCHOOL'],},
     ]
 }
 
@@ -16,16 +16,17 @@ const HTMLexamples = {
     title: 'HTML Examples', 
     group: 'HTMLexamples',
     cards: [
-        { name: 'Example Parrallax', image: 'Practice1.png', link: 'exampleparallax', datecreated: '25.11.22', tags: ['JS', 'EXAMPLE'],},
-        { name: 'Radial Array', image: 'RadialArray.png', link: 'radialarray', datecreated: '25.11.22', tags: ['JS', 'EXAMPLE'],},
-        { name: 'Kalkulator', image: 'kalkulator.png', link: 'kalkulator', datecreated: '15.01.23', tags: ['JS', 'LIBRARY', 'SCHOOL'],},
-        { name: 'Sparing', image: 'Sparing.png', link: 'sparing', datecreated: '20.12.22', tags: ['JS', 'LIBRARY', 'SCHOOL'],},
-        { name: 'Quiz', image: 'quiz.png', link: 'quiz', datecreated: '10.01.23', tags: ['JS', 'SCHOOL'],},
-        { name: 'Animation Test', image: 'animationtest.png', link: 'animationtest', datecreated: '19.01.23', tags: ['JS', 'LIBRARY'],},
-        { name: 'Array Oppgave', image: 'Array_Oppgave.png', link: 'arrayoppgave', datecreated: '24.01.23', tags: ['JS', 'SCHOOL'],},
-        { name: 'Carousel', image: 'Carousel.png', link: 'carousel', datecreated: '27.01.23', tags: ['JS', 'TEST'],},
-        { name: 'Blocks', image: 'blocks.png', link: 'blocks', datecreated: '01.02.23', tags: ['JS', 'TEST'],},
+        { name: 'Three.js', image: 'threejsapp.png', link: 'threeJS', datecreated: '04.03.23', tags: ['JS', 'TEST', 'LIBRARY']},
         { name: 'Text Effect', image: 'TextEffect.png', link: 'texteffect', datecreated: '21.02.23', tags: ['JS', 'EXAMPLE']},
+        { name: 'Blocks', image: 'blocks.png', link: 'blocks', datecreated: '01.02.23', tags: ['JS', 'TEST'],},
+        { name: 'Carousel', image: 'Carousel.png', link: 'carousel', datecreated: '27.01.23', tags: ['JS', 'TEST'],},
+        { name: 'Array Oppgave', image: 'Array_Oppgave.png', link: 'arrayoppgave', datecreated: '24.01.23', tags: ['JS', 'SCHOOL'],},
+        { name: 'Animation Test', image: 'animationtest.png', link: 'animationtest', datecreated: '19.01.23', tags: ['JS', 'LIBRARY'],},
+        { name: 'Quiz', image: 'quiz.png', link: 'quiz', datecreated: '10.01.23', tags: ['JS', 'SCHOOL'],},
+        { name: 'Sparing', image: 'Sparing.png', link: 'sparing', datecreated: '20.12.22', tags: ['JS', 'LIBRARY', 'SCHOOL'],},
+        { name: 'Kalkulator', image: 'kalkulator.png', link: 'kalkulator', datecreated: '15.01.23', tags: ['JS', 'LIBRARY', 'SCHOOL'],},
+        { name: 'Radial Array', image: 'RadialArray.png', link: 'radialarray', datecreated: '25.11.22', tags: ['JS', 'EXAMPLE'],},
+        { name: 'Example Parrallax', image: 'Practice1.png', link: 'exampleparallax', datecreated: '25.11.22', tags: ['JS', 'EXAMPLE'],},
     ]
 }
 
